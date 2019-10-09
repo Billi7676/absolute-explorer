09c7b900a8e3df2b77e1dc7a56a4f28d6eef84cb
CryptoCentric <cryptocentric@absolutecoin.net>
2019-10-09 18:02:33 +0100

    Card Link updates

--- client/component/Card/CardLinks.jsx
+++ client/component/Card/CardLinks.jsx
@@ -5,15 +5,15 @@ import Card from './Card';
 
 const CardLinks = () => (
   <Card title="Links">
-    <a href="https://bulwarkcrypto.com/" target="_blank" rel="nofollow noopener">Website</a><br />
-    <a href="https://bitcointalk.org/index.php?topic=2499481.0" target="_blank" rel="nofollow noopener">Bitcointalk</a><br />
-    <a href="https://github.com/bulwark-crypto" target="_blank" rel="nofollow noopener">Github</a><br />
-    <a href="https://www.reddit.com/r/BulwarkCoin/" target="_blank" rel="nofollow noopener">Reddit</a><br />
-    <a href="https://discord.gg/a7vhegP" target="_blank" rel="nofollow noopener">Discord</a><br />
-    <a href="https://t.me/bulwarkcrypto" target="_blank" rel="nofollow noopener">Telegram</a><br />
-    <a href="https://twitter.com/BulwarkCrypto" target="_blank" rel="nofollow noopener">Twitter</a><br />
-    <a href="https://medium.com/bulwark-cryptocurrency" target="_blank" rel="nofollow noopener">Medium</a><br />
-    <a href="https://community.bulwarkcrypto.com/" target="_blank" rel="nofollow noopener">Community Forum</a>
+    <a href="https://explorer.absolutecoin.net/" target="_blank" rel="nofollow noopener">Website</a><br />
+    <a href="https://bitcointalk.org/index.php?topic=4418859.0;topicseen" target="_blank" rel="nofollow noopener">Bitcointalk</a><br />
+    <a href="https://github.com/absolute-community/absolute/" target="_blank" rel="nofollow noopener">Github</a><br />
+    <a href="https://www.reddit.com/r/absolutecoin" target="_blank" rel="nofollow noopener">Reddit</a><br />
+    <a href="https://discord.gg/FhtgzY8" target="_blank" rel="nofollow noopener">Discord</a><br />
+    <a href="https://t.me/absolute_community" target="_blank" rel="nofollow noopener">Telegram</a><br />
+    <a href="https://twitter.com/Absolute_Crypto" target="_blank" rel="nofollow noopener">Twitter</a><br />
+    <a href="https://medium.com/@absolutecoin" target="_blank" rel="nofollow noopener">Medium</a><br />
+    <a href="https://bitcointalk.org/index.php?topic=4418859.0;topicseen/" target="_blank" rel="nofollow noopener">Community Forum</a>
   </Card>
 );
 

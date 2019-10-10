9fcaa90ab6bd674a0bfbf310aebdb4fd3caa161a
CryptoCentric <cryptocentric@absolutecoin.net>
2019-10-10 20:20:23 +0100

    Absoluted Setup Script update

--- script/absoluted_setup.sh
+++ script/absoluted_setup.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 # Download latest node and install.
 bwklink=`curl -s https://api.github.com/repos/absolute-community/absolute/releases/latest | grep browser_download_url | grep x86_64-linux | cut -d '"' -f 4`
+version=`curl -s https://api.github.com/repos/absolute-community/absolute/releases/latest | grep tag_name | grep v | cut -d '"' -f 4`
+versionsh=${version:1:7}
+sudo apt-get update -y -qq
+sudo apt-get upgrade -y -qq
+sudo apt-get install software-properties-common -y -qq
+sudo add-apt-repository ppa:bitcoin/bitcoin -y
+sudo apt-get update -y -qq	
+sudo apt-get install nano htop -y -qq
+sudo apt-get install pwgen  -y -qq	
+sudo apt-get install libdb4.8-dev libdb4.8++-dev -y -qq
+sudo apt-get install tmux  -y -qq
+sudo apt-get install libevent-pthreads-2.0-5 -y -qq
+sudo apt-get install libboost-all-dev -y -qq
+sudo apt-get install libzmq3-dev -y -qq
+sudo apt-get install libminiupnpc-dev -y -qq
+sudo apt install virtualenv -y -qq
+
 mkdir -p /tmp/absolute
 cd /tmp/absolute
 curl -Lo absolute.tar.gz $bwklink
 tar -xzf absolute.tar.gz
-sudo mv ./bin/* /usr/local/bin
+sudo mv ./absolutecore-0.$versionsh/bin/* /usr/local/bin
 cd
 rm -rf /tmp/absolute
 mkdir ~/.absolutecore
